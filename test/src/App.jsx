c1397a549ff352ac7991918068ccecb10e56d1d7
Rustam Guseinov <rg20031991.com>
2024-02-27 17:52:25 +0300

    add routing and some page commit

--- test/src/App.jsx
+++ test/src/App.jsx
@@ -1,10 +1,24 @@
 import "./App.css";
 import { ProductsPage } from "./pages/ProductsPage";
+import { HomePage } from "./pages/HomePage";
+import { NotFoundPage } from "./pages/NotFoundPage";
+import { Blog } from "./pages/Blog";
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <ProductsPage />
+      <header>
+        <Link to="/">Home</Link>
+        <Link to="/product">ProductsPage</Link>
+        <Link to="/posts">Blog</Link>
+      </header>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/product" element={<ProductsPage />} />
+        <Route path="/posts" element={<Blog />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
     </>
   );
 }
